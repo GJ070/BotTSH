9c365b3e3e6d8b244904d7a655b168bcca176f92
GJ070 <JesseVogel2004@hotmail.com>
2021-08-11 15:13:58 +0200

    Update test.js

--- Command/Testing/test.js
+++ Command/Testing/test.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js'); 
 const client = new Discord.Client(); 
+const d = new Date();
 
 module.exports = {
     name: 'test',
     description: 'gives FAQ',
     async execute (message, args){
         message.delete();
-
+        d.toDateString()
     }
 }
